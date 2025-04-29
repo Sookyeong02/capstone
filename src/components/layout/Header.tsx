542ec4db002b72c75154e7c220f75f1a3eb95c14
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2025-04-29 13:37:58 +0900

    fix: 로그인, 회원가입 페이지에서 숨김 처리

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,23 @@
+'use client'; 
+
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "../../../public/icons/logo.svg";
 import Bell from "../../../public/icons/bell.svg";
-import SerchIcon from "../../../public/icons/serch.svg";
+import SearchIcon from "../../../public/icons/search.svg";
 import User from "../../../public/icons/user.svg";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
+  const hideHeaderPaths = ['/login', '/signup'];
+  const shouldHideHeader = hideHeaderPaths.includes(pathname);
+
+  if (shouldHideHeader) {
+    return null;  // 렌더링 아예 안 함
+  }
+
   return (
     <header className="w-full h-[70px] flex justify-center items-center bg-[#ffffff] border-b border-gray-200">
       <nav className="w-full max-w-[1600px] h-[30px] flex justify-between items-center px-[40px]">        
@@ -30,8 +42,8 @@ export default function Header() {
           </div>
 
           <div className="hidden md:flex items-center gap-[4px] md:gap-[10px] text-2lg font-bold">
-            <Image width={18} height={18} src={SerchIcon} alt="검색" />
-            <p>Serch</p>
+            <Image width={18} height={18} src={SearchIcon} alt="검색" />
+            <p>Search</p>
           </div>
 
           <div className="flex items-center gap-[4px] md:gap-[10px] text-md md:text-2lg font-bold">
