69fd9506cc5071ab3a64867fe465e6756d978fa5
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2025-04-30 13:58:25 +0900

    fix: type 수정

--- src/types/Jobs.ts
+++ src/types/Jobs.ts
@@ -3,11 +3,12 @@ export interface Job {
   id: string;
   companyId: string;  
   title: string;
-  category?: string;
+  category: string;
+  experience: string,
   content?: string;
   link: string;
   deadline: string;
-  location?: string;
+  location: string;
   thumbnail?: string;
   createdAt: string;
   updatedAt: string;
